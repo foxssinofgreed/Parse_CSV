7db52b64c3ac04a54b375046eabd071a23962d8d
Fox <l_aptsiauri@cu.edu.ge>
2021-11-17 13:56:34 +0400

    update from linux

---
+++ cmake-build-debug/CMakeFiles/Parse_CSV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Parse_CSV.dir/main.cpp.o"
+  "Parse_CSV"
+  "Parse_CSV.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Parse_CSV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
